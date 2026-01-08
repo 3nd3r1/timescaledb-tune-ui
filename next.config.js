028e0b130f36e26a353717caa588b34b15254d75
3nd3r1 <ender.spam.guru@gmail.com>
2026-01-08 16:16:41 +0200

    fix(build): fix the dockerfile

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     typedRoutes: true,
-    experimental: {
-        optimizeCss: true,
-    },
     compress: true,
     poweredByHeader: false,
     generateEtags: true,
